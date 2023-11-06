74076638c94dec46949f1ef980e757c104efd085
Devmatrix <devmatrix73@gmail.com>
2023-11-06 23:53:07 +0100

    closes #1 - Add Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,19 @@
 import {NavLink} from "react-router-dom";
+import logo from "../../assets/images/logo.svg";
 
 function Header() {
   return (
     <>
-      <header>
-        <nav>
-          <NavLink to="/">Accueil</NavLink>
-          <NavLink to="/about">A propos</NavLink>
-        </nav>
+      <header className="header-site">
+        <div>
+          <a href="#">
+            <img src={logo} alt="Kasa" width="100" height="auto"/>
+          </a>
+          <nav>
+            <NavLink to="/">Accueil</NavLink>
+            <NavLink to="/about">A propos</NavLink>
+          </nav>
+        </div>
       </header>
     </>
   );
