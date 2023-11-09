257dc9280b165dabf01ef220c9c458d35fcda326
Devmatrix <devmatrix73@gmail.com>
2023-11-09 18:52:06 +0100

    Ref. #8 - Update About page with Collapse component

---
+++ src/datas/aboutList.jsx
@@ -0,0 +1,18 @@
+export const aboutList = [
+  {
+    title: 'Fiabilité',
+    description: 'Lorem ipsum 1 dolor sit amet, consectetur adipiscing elit. Phasellus erat magna, tincidunt vel ipsum non, venenatis commodo ipsum. Ut iaculis sem eu nisi suscipit elementum. Cursus hendrerit eleifend.'
+  },
+  {
+    title: 'Respect',
+    description: 'Lorem ipsum 2 dolor sit amet, consectetur adipiscing elit. Phasellus erat magna, tincidunt vel ipsum non, venenatis commodo ipsum. Ut iaculis sem eu nisi suscipit elementum. Cursus hendrerit eleifend.'
+  },
+  {
+    title: 'Service',
+    description: 'Lorem ipsum 3 dolor sit amet, consectetur adipiscing elit. Phasellus erat magna, tincidunt vel ipsum non, venenatis commodo ipsum. Ut iaculis sem eu nisi suscipit elementum. Cursus hendrerit eleifend.'
+  },
+  {
+    title: 'Sécurité',
+    description: 'Lorem ipsum 4 dolor sit amet, consectetur adipiscing elit. Phasellus erat magna, tincidunt vel ipsum non, venenatis commodo ipsum. Ut iaculis sem eu nisi suscipit elementum. Cursus hendrerit eleifend.'
+  }
+];
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,4 +1,6 @@
 import Banner from '../../components/Banner';
+import Collapse from '../../components/Collapse';
+import { aboutList } from '../../datas/aboutList'
 
 function About() {
 
@@ -8,6 +10,14 @@ function About() {
         title = 'A propos' />
       <div className="about">
         <div>
+          {
+            aboutList.map((item, index) =>              
+              <Collapse 
+                key = {`id-${index}`}
+                number = {index}
+                text = {item.description}
+              />)
+          }
         </div>
       </div>
     </>
