6a4bee7c63d99aeb1be74301502b3fa2715fa457
Devmatrix <devmatrix73@gmail.com>
2023-11-14 02:15:49 +0100

    Ref. #10 - Update property page with host, stars + update tags

--- src/pages/Property/index.jsx
+++ src/pages/Property/index.jsx
@@ -9,25 +9,41 @@ function Property() {
   let property = propertiesData
     .find((data) => data.id == propertyId);
 
-  let tags = property.tags;
-  tags.map((tag) => console.log(tag))
+  let tags = property.tags
+    .map((tag, index) => <li key={`tag-${index}`}>{tag}</li>)
 
+  let host = property.host;
+  
   return (
     <>
     <div className='property'>
       <div>
-        <h1>
-          {property.title}
-        </h1>
-        <h2>
-          {property.location}
-        </h2>
-        <ul>
-          {
-          tags.map((tag, index) => 
-            `<li key=tag-${index}>${tag}</li>`)
-          }
-        </ul>
+        <div className='part-left'>
+          <h1>
+            {property.title}
+          </h1>
+          <h2>
+            {property.location}
+          </h2>
+          <ul>
+            {tags}
+          </ul>
+        </div>
+        <div className='part-right'>
+          <div className='part-right__host'>
+            <span>
+              {host.name}
+            </span>
+            <img aria-hidden="true" src={host.picture} alt={host.name} />
+          </div>
+          <div className='part-right__rating'>
+            <i className='fa-solid fa-star' aria-hidden='true'></i>
+            <i className='fa-solid fa-star' aria-hidden='true'></i>
+            <i className='fa-solid fa-star' aria-hidden='true'></i>
+            <i className='fa-solid fa-star' aria-hidden='true'></i>
+            <i className='fa-solid fa-star' aria-hidden='true'></i>
+          </div>
+        </div>
       </div>
     </div>
     </>
